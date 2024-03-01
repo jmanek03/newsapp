4d596d61c5255be5bb43865d8185c566f9b3e713
Jay Manek <manekj27@gmail.com>
2024-03-01 22:41:32 +0530

    Add a News API 1-3-24

--- src/components/News.js
+++ src/components/News.js
@@ -4,13 +4,20 @@ import NewsItem from './NewsItem'
 export class News extends Component {
   render() {
     return (
-      <div>
-        This is a news Component
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
+      <div className='container my-3'>
+        <h2>NewsApp - Top Headlines</h2>
+        <div className='row'>
+          <div className="col-md-4">
+            <NewsItem title="myTitle1" description="myDesc1"/>
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="myTitle2" description="myDesc2"/>
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="myTitle3" description="myDesc3"/>
+          </div>        
+        
+        </div>
       </div>
     )
   }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,9 +2,17 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
+    let {title,description}=this.props;
     return (
       <div>
-        This is news item.
+        <div classname="card" style={{width: "18rem"}}>
+            <img src="..." classname="card-img-top" alt="..."/>
+            <div classname="card-body">
+              <h5 classname="card-title">{title}</h5>
+              <p classname="card-text">{description}</p>
+              {/* <a href="#" classname="btn btn-primary">Go somewhere</a> */}
+            </div>
+        </div>
       </div>
     )
   }
