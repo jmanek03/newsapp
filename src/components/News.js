c352ec95348d0e6b39903c59b05cd15709e2dd4a
Jay Manek <manekj27@gmail.com>
2024-03-06 22:55:03 +0530

    Add a Fetch API 6-3-24

--- src/components/News.js
+++ src/components/News.js
@@ -271,13 +271,20 @@ export class News extends Component {
       loading:false
     }
   }
+  async componentDidMount(){
+    let url ="https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=6f4a1b34ac914726b4d5f620d232a6af";
+    let data= await fetch(url);
+    let parsedData= await data.json();
+    this.setState({articles:parsedData.articles})
+
+  }
   render() {
     return (
       <div className="container my-3">
-        <h2>NewsApp - Top Headlines</h2>
+        <h1>NewsApp - Top Headlines</h1>
         
         <div className='row'>
-        {this.state.articles.map((element)=>{
+        {this.state.articles?.map((element)=>{
           return <div className="col-md-4" key={element.url}>
           <NewsItem title={element.title.slice(0,45)} description={element.description?.slice(0,50)} imageUrl={element.urlToImage} newsUrl={element.url}/>
         </div>
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -7,7 +7,7 @@ export class NewsItem extends Component {
     return (
       <div className="my-4">
         <div className="card" style={{width: "18rem"}}>
-            <img src={imageUrl} className="card-img-top" alt="..."/>
+            <img src={!imageUrl?"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIQuSjiMrxhmvRSl-5IS9ZoA99ueIGIX84SQ&usqp=CAU":imageUrl} className="card-img-top" alt="..."/>
             <div className="card-body">
               <h5 className="card-title">{title}...</h5>
               <p className="card-text">{description}...</p>
