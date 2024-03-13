c84e5100bd5acf5919575942d0684a5980ab2ead
Jay Manek <manekj27@gmail.com>
2024-03-14 00:21:41 +0530

    Add a React router using react-router-dom 14-3-24

--- src/App.js
+++ src/App.js
@@ -3,13 +3,29 @@ import './App.css';
 import React, { Component } from 'react'
 import Navbar from './components/Navbar';
 import News from './components/News';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 export default class App extends Component {
   render() {
     return (
       <div>
+        <Router>
         <Navbar/>
-        <News pageSize={5} counry="in" category="Sports"/>
+        
+        <Routes>
+          <Route exact path="/" element={<News key="general" pageSize={5} counry="in" category="general"/>}></Route>
+          <Route exact path="/business" element={<News key="business" pageSize={5} counry="in" category="business"/>}></Route>
+          <Route exact path="/entertainment" element={<News key="entertainment" pageSize={5} counry="in" category="entertainment"/>}></Route>
+          <Route exact path="/health" element={<News key="health" pageSize={5} counry="in" category="health"/>}></Route>
+          <Route exact path="/science" element={<News key="science" pageSize={5} counry="in" category="science"/>}></Route>
+          <Route exact path="/sports" element={<News key="sports" pageSize={5} counry="in" category="sports"/>}></Route>
+          <Route exact path="/technology" element={<News key="technology" pageSize={5} counry="in" category="technology"/>}></Route>
+        </Routes>
+        </Router>
       </div>
     )
   }
