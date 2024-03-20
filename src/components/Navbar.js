f8cd6c3c369e8170d111f4fc8fca0690c07ad7c4
Jay Manek <manekj27@gmail.com>
2024-03-20 09:56:32 +0530

    Small errors 20-3-24

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,11 +9,11 @@ export class Navbar extends Component {
       <div>
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark ">
         <div className="container-fluid">
-            <Link className="navbar-brand" to="/">NewsApp</Link>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <Link className="navbar-brand fs-2" to="/">NewsApp</Link>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
             </button>
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <div className="collapse navbar-collapse fs-4" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                 <li className="nav-item navbar-dark bg-dark">
                 <Link className="nav-link" aria-current="page" to="/">Home</Link>
@@ -23,7 +23,7 @@ export class Navbar extends Component {
                 </li>
                 <li className="nav-item navbar-dark bg-dark">
                 <Link className="nav-link" to="/entertainment">Entertainment</Link>
-                </li>
+                </li> 
                 <li className="nav-item navbar-dark bg-dark">
                 <Link className="nav-link" to="/health">Health</Link>
                 </li>
